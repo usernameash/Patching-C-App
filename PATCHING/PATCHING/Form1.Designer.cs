ecb3d8fac43602854138ff1e5b60828ef0c7337f
Ash <84189626+usernameash@users.noreply.github.com>
2021-06-08 09:23:44 +0100

    Added files via upload
    Added files via upload as the project was created before downloading git.bash etc. This is fro porting over to another PC and testing.

---
+++ PATCHING/PATCHING/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PATCHING
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Servers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 470);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.ForeColor = System.Drawing.Color.Green;
+            this.textBox2.Location = new System.Drawing.Point(200, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(504, 450);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Address List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(732, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PATCHING/PATCHING/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PATCHING
+{
+    public partial class Form1 : Form
+    {
+        // get these from config
+        string username;
+        string password;
+        List<serverConfig> Servers = new List<serverConfig>();
+        public List<string> Addresses_to_check { get; set; } = new List<string>();
+
+
+
+        // Make a config file for crednetials
+
+
+
+        public Form1()
+        {
+            // 
+
+            var settings = ConfigurationManager.AppSettings;
+
+            foreach(var line in settings.AllKeys)
+            {
+
+                if (line.StartsWith("addr,"))
+                {
+                    var address = line.Split(',')[1];
+                    var servertype = settings.Get(line);
+                    Servers.Add(new serverConfig { 
+                        Address = address,
+                        commands = settings.Get(servertype).Split(';')}
+                    );
+
+                }
+            }
+
+             username = settings.Get("username");
+             password = settings.Get("password");
+
+
+
+
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var list = textBox1.Text;
+            // trim input strings 
+            Addresses_to_check = list.Split('\n').ToList();
+            Char[] charsToTrim = { '\r' };
+
+            // Start connecting to the server/node
+            startcheckin();
+
+            
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Enter the field of servers you are wanting to check or to be checked
+        }
+
+       private void startcheckin ()
+        {
+            foreach (var address in Addresses_to_check)
+            
+            {
+                serverConfig config = Servers.First(server => server.Address == address);
+
+
+                var Client = new Renci.SshNet.SshClient(address, username, password);
+                Client.Connect();
+                foreach (string command in config.commands)
+                {
+                   var cmd = Client.RunCommand(command);
+                    textBox2.Text +=($"{cmd.Result}\r\n");
+                }
+                Client.Disconnect();
+                // find statements 
+                // write command response to text field
+                // execute commands
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
